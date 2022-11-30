5c9211b0a32a5e5fca9ca8e29c20fa7135f14344
hemanth1480 <hemanth.m1480@gmail.com>
2022-11-30 10:54:37 +0530

    u

--- mail.js
+++ mail.js
@@ -3,18 +3,18 @@ var nodemailer = require('nodemailer');
 
 const sendmail = (email,token) => {
     const msg = {
-        from: "hemanth.m1480@gmail.com",
-        to: "hemanth.m1480@gmail.com",
+        from: "",
+        to: "",
         subject: "Password change request accepted",
         text: "Use the link provided to change our account password " +
-        "http://localhost:59/accountVerification?verifyid=" + token
+        "" + token
     };
     
     nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: "hemanth.m1480@gmail.com",
-            pass: "frsvmfxhljxckees"
+            user: "",
+            pass: ""
         },
         port:465,
         host: "smtp.gmail.com"

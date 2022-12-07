0278d68a440849a2a3b12a6d7dcc3a8e62c255ee
hemanth1480 <hemanth.m1480@gmail.com>
2022-12-07 16:18:36 +0530

    changes in download data

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const fs = require("fs");
 var bson = require("bson");
 const sendmail = require("./mail.js");
+const { get } = require("http");
 require('dotenv').config();
+const zip = require('express-zip');
 
 const saltRounds = parseInt(process.env.SLT);
 const sR = parseInt(process.env.sR);
@@ -179,7 +181,7 @@ app.get("/data/store/api", (req, res) => {
                     if (!(bson.calculateObjectSize(dat) > 7000000)) {
                         Data.updateOne({id: req.query.id}, {$push: {para1: req.query[dat[0].xLabel],para2: req.query[dat[0].yLabel],para3: req.query[dat[0].zLabel],para4: req.query[dat[0].hLabel],timeStamp:newDate}}, (err, tres) => {
                             if (err) {
-                                res.redirect("/login");
+                                res.redirect("/error");
                             } else {
                                 res.send("Uploaded");
                             }
@@ -313,20 +315,47 @@ app.get("/accountVerification", (req,res) => {
     });
 });
 
-app.get("/enlarge", (req,res) => {
+// app.get("/enlarge", (req,res) => {
+//     if (req.session.userId) {
+//         Data.find({id:req.query.id}, (err,output) => {
+//             if (err) {
+//                 res.redirect("/error")
+//             } else if (output.length != 0) {
+//                 res.render("enlarge/enlarge",{inp:output});
+//             } else {
+//                 res.redirect("/login")
+//             }
+//         })
+//     } else {
+//         res.redirect("/login?forward=enlarge&id=" + req.query.id);
+//     }
+// });
+
+app.get("/processData", (req,res) => {
     if (req.session.userId) {
-        Data.find({id:req.query.id}, (err,output) => {
+        var id = req.query.graphid.split("?")[0];
+        Data.find({_id:id}, (err,data) => {
             if (err) {
-                res.redirect("/error")
-            } else if (output.length != 0) {
-                res.render("enlarge/enlarge",{inp:output})
+                res.redirect("/");
+            } else if (data.length !=0 ) {
+                res.render("enlarge/enlarge",{inp:data});
             } else {
-                res.redirect("/login")
+                res.send("Graph not found");
             }
-        })
+        });
     } else {
-        res.redirect("/login?forward=enlarge&id=" + req.query.id);
+        res.redirect("/login?forward=processData?graphid=" + req.query.graphid);
     }
+});
+
+app.get("/back", (req,res) => {
+    Data.updateOne({id:"958b8a7f4454e1e99700cd9372646d1d"},{$push:{para1:87000}}, (err,kk) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send("done")
+        }
+    })
 });
 
 app.post("/register", (req, res) => {
@@ -402,7 +431,6 @@ app.post("/labelchange", (req,res) => {
                     if (er) {
                         res.redirect("/error");
                     } else {
-                        console.log(dres);
                         if (req.query.id == null) {
                             res.redirect("/stored-data");
                         } else {
@@ -438,25 +466,103 @@ app.post("/download-data", (req,res) => {
                 for(var c=1; c <= xval.length;c++) {
                     len.push(c);
                 }
+                
                 const csvWriter = createCsvWriter({
                     path:__dirname + "/download-data/" + uno[0]._id + "/" + uno[0].title + ".csv",
                     header: [
                         {id: 'number', title: 'S.No'},
-                        {id:'time', title: 'time'},
-                        {id: 'xValue', title: 'xValue'},
-                        {id: 'yValue', title: 'yValue'},
-                        {id: 'zValue', title: 'zValue'},
-                        {id: 'hValue', title: 'hValue'}
+                        {id:'Timestamp',title:'Timestamp'},
+                        {id:'Date', title: 'Date'},
+                        {id:'time',title: "time"},
+                        {id: 'xValue', title: uno[0].xLabel},
+                        {id: 'yValue', title: uno[0].yLabel},
+                        {id: 'zValue', title: uno[0].zLabel},
+                        {id: 'hValue', title: uno[0].hLabel}
                     ]
                 });
                 const records = [];
+                var dates = [];
                 for (let l = 0; l < xval.length; l++) {
-                    var sub = {number:len[l],xValue:xval[l],yValue:yval[l],zValue:zval[l],hValue:hval[l],time:uno[0].timeStamp[l]}
+                    dates.push(uno[0].timeStamp[l].split("T")[0]);
+                    var sub = {number:len[l],xValue:xval[l],yValue:yval[l],zValue:zval[l],hValue:hval[l],Date:uno[0].timeStamp[l].split("T")[0],time:uno[0].timeStamp[l].split("T")[1],Timestamp:uno[0].timeStamp[l]}
                     records.push(sub);
+                }
+                var uniqDates = [ ...new Set(dates)];
+                var newx = [];
+                var newy = [];
+                var newz = [];
+                var newh = [];
+                var inputLabels = [];
+                inputLabels.push(
+                    {id: 'number', title: 'S.No'},
+                    {id:'Date', title: 'Date'},
+                    {id: 'xMin', title: uno[0].xLabel + "Min"},
+                    {id: 'xMax', title: uno[0].xLabel + "Max"},
+                    {id: 'xAvg', title: uno[0].xLabel + "Avg"}
+                );
+                if (!(uno[0].para2.length <=1) ) {
+                    inputLabels.push({id: 'yMin', title: uno[0].yLabel + "Min"},
+                    {id: 'yMax', title: uno[0].yLabel + "Max"},
+                    {id: 'yAvg', title: uno[0].yLabel + "Avg"})
+                } if(!(uno[0].para3.length <=1)) {
+                    inputLabels.push({id: 'zMin', title: uno[0].zLabel + "Min"},
+                    {id: 'zMax', title: uno[0].zLabel + "Max"},
+                    {id: 'zAvg', title: uno[0].zLabel + "Avg"})
+                } if (!(uno[0].para4.length <=1)) {
+                    inputLabels.push({id: 'hMin', title: uno[0].hLabel + "Min"},
+                    {id: 'hMax', title: uno[0].hLabel + "Max"},
+                    {id: 'hAvg', title: uno[0].hLabel + "Avg"})
+                }
+                const csvWriter1 = createCsvWriter({
+                    path:__dirname + "/download-data/" + uno[0]._id + "/" + "dateSeries-" + uno[0].title + ".csv",
+                    header: inputLabels
+                });
+                var newData = [];
+                for (let h = 0; h < uniqDates.length; h++) {
+                    for(let s=0; s < xval.length; s++) {
+                        if (uniqDates[h] == uno[0].timeStamp[s].split("T")[0]) {
+                            newx.push(uno[0].para1[s]);
+                            newy.push(uno[0].para2[s]);
+                            newz.push(uno[0].para3[s]);
+                            newh.push(uno[0].para4[s]);
+                        }
+                    }
+                    var resultx = newx.filter(element => {
+                        return element !== undefined;
+                    });
+                    var resulty = newy.filter(element => {
+                        return element !== undefined;
+                    });
+                    var resultz = newz.filter(element => {
+                        return element !== undefined;
+                    });
+                    var resulth = newh.filter(element => {
+                        return element !== undefined;
+                    });
+                    var nw = {number:h,Date:uniqDates[h],xMin:Math.min(...resultx),xMax:Math.max(...resultx),xAvg:(Math.max(...resultx)+Math.min(...resultx))/2,yMin:Math.min(...resulty),yMax:Math.max(...resulty),yAvg:(Math.max(...resulty)+Math.min(...resulty))/2,zMin:Math.min(...resultz),zMax:Math.max(...resultz),zAvg:(Math.max(...resultz)+Math.min(...resultz))/2,hMin:Math.min(...resulth),hMax:Math.max(...resulth),hAvg:(Math.max(...resulth)+Math.min(...resulth))/2}
+                    newData.push(nw);
+                    newx.length=0;
+                    newy.length=0;
+                    newz.length=0;
+                    newh.length=0;
+                    resultx.length=0;
+                    resulty.length=0;
+                    resultz.length=0;
+                    resulth.length=0;
                 }
                 csvWriter.writeRecords(records) 
                 .then(() => {
-                    res.download(__dirname + "/download-data/" + uno[0]._id + "/" + uno[0].title + ".csv");
+                    csvWriter1.writeRecords(newData) 
+                    .then(() => {
+                        // var download = [__dirname + "/download-data/" + uno[0]._id + "/" + uno[0].title + ".csv",__dirname + "/download-data/" + uno[0]._id + "/" + "dateSeries-" + uno[0].title + ".csv"];
+                        // download.forEach( one => {
+                        //     res.download(one);
+                        // });
+                        res.zip([
+                            { path: 'download-data/635d649967c5564e41e5cf95/dateSeries-sens.csv', name: 'dateSeries-sens.csv' },
+                            { path: 'download-data/635d649967c5564e41e5cf95/sens.csv', name: 'sens.csv' }
+                          ]);
+                    });
                 });
             }
         })
